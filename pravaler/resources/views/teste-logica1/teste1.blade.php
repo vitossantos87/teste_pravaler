26277ef9809933dafa761ff9026472f7b3e7b0ca
vitor <vitor.santos@ambragaming.com>
2019-12-11 17:07:25 -0300

    estruturando vies

---
+++ pravaler/resources/views/teste-logica1/teste1.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.principal')
+@section('title', 'Teste 1')
+@section('styles')
+    @parent
+@stop
+    @section('conteudo')
+            <div class="content">
+                <div class="title m-b-md">
+                    Desafio Pravaler
+                </div>
+
+
+            </div>
+@stop
+
+@section('scripts')
+    @parent
+@stop
