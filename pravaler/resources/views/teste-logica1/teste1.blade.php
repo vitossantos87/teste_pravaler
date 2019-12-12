ee31d6fd23a88638da99d8d92b9bd8ab425d47a9
Vitor Santos <marrudols@gmail.com>
2019-12-11 21:44:08 -0300

    fazendo teste de loógica1

--- pravaler/resources/views/teste-logica1/teste1.blade.php
+++ pravaler/resources/views/teste-logica1/teste1.blade.php
@@ -6,13 +6,44 @@
     @section('conteudo')
             <div class="content">
                 <div class="title m-b-md">
-                    Desafio Pravaler
+                    Desafio Pravaler: Teste 1
                 </div>
+                <form>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1">Produto</span>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Nome do Produto" aria-label="Produto" id="Produto" aria-describedby="basic-addon1">
+                    </div>
 
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon2">Quantidade</span>
+                        </div>
+                        <input type="number" class="form-control" placeholder="Quantidade do Produto" aria-label="quantidade" id="quantidade" aria-describedby="basic-addon2">
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon3">Preço Unitário</span>
+                        </div>
+                        <input type="number" class="form-control" placeholder="Preço Unitário" aria-label="preco" id="preco" aria-describedby="basic-addon3">
+                    </div>
+
+                    <button type="button" class="btn btn-primary" onclick="calculaValorDesconto();">Calcular</button>
+
+                    <ul class="list-group" id="calculado">
+                        <li class="list-group-item">Total: <span id="total"> </span> </li>
+                        <li class="list-group-item">Desconto: <span id="desconto"></li>
+                        <li class="list-group-item">Total a pagar: <span id="total_pagar"></li>
+                    </ul>
+
+                </form>
 
             </div>
 @stop
 
 @section('scripts')
     @parent
+    <script src="{{ asset('js/teste-logica1.js')}}"></script>
 @stop
--- pravaler/routes/web.php
+++ pravaler/routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste-logica-1', 'TesteLogica1Controller@index')->name('teste1');
+Route::get('/teste-logica-2', 'TesteLogica2Controller@index')->name('teste2');
