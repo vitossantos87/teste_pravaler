fd1f0b5acd20236c370a753655d452182553baa7
Vitor Santos <marrudols@gmail.com>
2019-12-15 11:31:14 -0300

    instalando sistema de autenticacao do laravel

--- pravaler/resources/views/welcome.blade.php
+++ pravaler/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.principal')
+@extends('layouts.principal')
 @section('title', 'Home')
 @section('styles')
     @parent
@@ -12,7 +12,7 @@
                 <div class="links">
                     <a href="{{route('teste1')}}">Teste de Lógica 1</a>
                     <a href="{{route('teste2')}}">Teste de Lógica 2</a>
-                    <a href="https://laravel-news.com">Sistema SGIE</a>
+                    <a href="https://laravel-news.com">Sistema SIGIE</a>
                 </div>
             </div>
 @stop
